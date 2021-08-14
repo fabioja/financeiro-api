5b680ded63a599d932c7176d604eca425cf5d581
Fábio José Araújo <fabioja@gmail.com>
2021-08-13 21:40:56 -0300

    incluído paginação dos lançamentos

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -3,15 +3,17 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
 import com.example.algamoney.api.service.LancamentoService;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -30,8 +32,8 @@ public class LancamentoResource {
     }
 
     @GetMapping
-    public List<Lancamento> listar() {
-        return lancamentoRepository.findAll();
+    public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+        return lancamentoRepository.filtrar(lancamentoFilter, pageable);
     }
 
     @PostMapping
@@ -47,5 +49,11 @@ public class LancamentoResource {
     public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
         Optional<Lancamento> optional = lancamentoRepository.findById(codigo);
         return optional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerCodigo(@PathVariable Long codigo) {
+        lancamentoRepository.deleteById(codigo);
     }
 }
